30404f8cf719905887bac33618e9bf8ae3436a8d
nekomura <>
2024-08-13 22:02:00 +0900

    update : init commit

---
+++ app.js
@@ -0,0 +1,62 @@
+const {createBluetooth} = require('node-ble')
+
+function wait(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+async function scanDevice(devName,UUID){
+    const {bluetooth, destroy} = createBluetooth();
+    const adapter = await bluetooth.defaultAdapter();
+    const targetDevice = [];
+    if (! await adapter.isDiscovering()) {
+      console.log('Discovering:');
+      await adapter.startDiscovery();
+      await wait(3000);
+      console.log(await adapter.toString());
+      const devices = await adapter.devices();
+      if(devices.length != 0){
+        let validate = null;
+        for(let i=0; devices.length>i; i++){
+            try {
+                validate = await adapter.getDevice(devices[i]);
+                validateService = await validate.helper.props();
+                await validate.helper.removeAllListeners();
+                console.log("service condition", validateService);
+                if(validateService.Name === devName && validateService.UUIDs[0] === UUID){
+                    targetDevice.push("success");
+                    targetDevice.push(validateService.Address);
+                    if('RSSI' in validateService){
+                        targetDevice.push(validateService.RSSI);
+                    }else{
+                        targetDevice.push("RSSI ERROR");
+                    }
+                }
+            } catch (error) {
+                console.log("scan get error");
+                 targetDevice.push("scan get error");
+            }
+            validate = null;
+            }
+        }
+        else{
+            console.log("no Device aria");
+            targetDevice.push("no Device aria");
+        }
+        await adapter.stopDiscovery();
+    }
+    destroy();
+    if(targetDevice.length === 0){
+        targetDevice.push("target not find");
+    }
+    return targetDevice
+}
+
+async function main() {
+    while(true){
+        const reslut = await scanDevice('deviceName','UUID');
+        console.log(`result ${reslut}`);
+    }
+    
+}
+
+main();
---
+++ setup.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+source ~/.nvm/nvm.sh
+nvm use v18.20.3
+npm install
